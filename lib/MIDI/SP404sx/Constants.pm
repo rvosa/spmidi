58043a7b7ca6d326506017e5dff38da8879b8e4e
Rutger Vos <rutgeraldo@gmail.com>
2021-04-03 00:15:36 +0200

    added pattern getter/setters

--- lib/MIDI/SP404sx/Constants.pm
+++ lib/MIDI/SP404sx/Constants.pm
@@ -13,6 +13,10 @@ our $pad_offset_magic_number = 46;
 our $max_velocity = 127;
 our $max_note = 127;
 our $max_channel = 15;
+our $min_bpm = 40;
+our $max_bpm = 200;
+our $min_length = 1;
+our $max_length = 99;
 our $path_to_samples = "./SP-404SX/SMPL/";
 our $length_encoding = "008C000000000000\n00{}000000000000";
 
--- lib/MIDI/SP404sx/Pattern.pm
+++ lib/MIDI/SP404sx/Pattern.pm
@@ -1,6 +1,35 @@
-package cd scalar();
+package MIDI::SP404sx::Pattern;
 use strict;
 use warnings;
+use MIDI::SP404sx::Constants;
 use base 'MIDI::SP404sx';
+
+sub tempo {
+    my $self = shift;
+    if ( @_ ) {
+        my $val = shift;
+        if ( $val >= $MIDI::SP404sx::Constants::min_bpm && $val <= $MIDI::SP404sx::Constants::max_bpm ) {
+            $self->{tempo} = $val;
+        }
+        else {
+            die $val;
+        }
+    }
+    return $self->{tempo};
+}
+
+sub nlength {
+    my $self = shift;
+    if ( @_ ) {
+        my $val = shift;
+        if ( $val >= $MIDI::SP404sx::Constants::min_length && $val <= $MIDI::SP404sx::Constants::max_length ) {
+            $self->{nlength} = $val;
+        }
+        else {
+            die $val;
+        }
+    }
+    return $self->{nlength};
+}
 
 1;
