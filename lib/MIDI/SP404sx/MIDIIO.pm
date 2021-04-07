04facd7b6d2bfb13bcf6435d64aff16b024f79a4
Rutger Vos <rutgeraldo@gmail.com>
2021-04-07 19:57:19 +0200

    implemented midi export

--- lib/MIDI/SP404sx/MIDIIO.pm
+++ lib/MIDI/SP404sx/MIDIIO.pm
@@ -7,8 +7,6 @@ use MIDI::SP404sx::Note;
 use MIDI::SP404sx::Pattern;
 use Log::Log4perl qw(:easy);
 
-Log::Log4perl->easy_init($DEBUG);
-
 my $type     = 0;
 my $dtime    = 1;
 my $channel  = 2;
@@ -18,6 +16,7 @@ my $velocity = 4;
 sub read_midi {
     my $file = shift;
     my $opus = MIDI::Opus->new({ from_file => $file });
+    #print Dumper($opus);
     return decode_events($opus);
 }
 
@@ -29,7 +28,6 @@ sub decode_events {
         my $position = 0;
         EVENT: for my $e ( $track->events ) {
             next EVENT unless $e->[$type] =~ /(note|set_tempo)/;
-            DEBUG Dumper($e);
             if ( $e->[$type] eq 'set_tempo' ) {
                 next EVENT;
             }
@@ -62,6 +60,48 @@ sub decode_events {
         }
     }
     return $pattern;
+}
+
+sub write_midi {
+    my ( $pattern, $file ) = @_;
+    my $ppqn = $MIDI::SP404sx::Constants::PPQ;
+    my $opus = MIDI::Opus->new({ format => 1, ticks => $ppqn });
+    my @tracks;
+    for my $c ( sort { $a <=> $b } keys %{{ map { $_->channel => 1 } $pattern->notes }}) {
+        my ( $track, $position, @offs ) = ( MIDI::Track->new, 0 );
+        my @events = ( [ 'track_name', 0, scalar(@tracks) == 0 ? 'Base channel' : 'Upper channel' ] );
+        for my $n ( sort { $a->position <=> $b->position } grep { $_->channel == $c } $pattern->notes ) {
+
+            # resolve any dangling note-off events
+            my @noffs;
+            for my $o ( sort { $a->{position} <=> $b->{position} } @offs ) {
+                if ( $o->{position} <= $n->position ) {
+                    $o->{event}->[$dtime] = int( ( $o->{position} - $position ) * $ppqn );
+                    $position = $o->{position};
+                    push @events, $o->{event};
+                }
+                else {
+                    push @noffs, $o;
+                }
+            }
+            @offs = @noffs;
+
+            # create and insert note on
+            my $delta_t = int( ( $n->position - $position ) * $ppqn );
+            my @on = ( 'note_on', $delta_t, $n->channel, $n->pitch, $n->velocity );
+            push @events, \@on;
+
+            # queue dangling note-off event
+            my @off = @on;
+            $off[$type] = 'note_off';
+            push @offs, { position => $n->position + $n->nlength, event => \@off };
+        }
+        $track->events(@events);
+        push @tracks, $track;
+    }
+    $opus->tracks(@tracks);
+    #print Dumper($opus);
+    $opus->write_to_file($file);
 }
 
 1;
